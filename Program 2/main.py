aadb63b400cecbb3fef0f8671ca3220d4e15d53c
Rayos1 <emc1140@hotmail.com>
2023-11-03 10:00:20 -0500

    .

---
+++ Program 2/main.py
@@ -0,0 +1,12 @@
+
+class Course():
+    
+    def __init__(self, name, enrollement, preferred, facilitators, others):
+        
+        self.name = name
+        self.enrollment = enrollement
+        self.preferred = preferred
+        self.facilitators = facilitators
+        self.others = others
+        
+def main(): pass
